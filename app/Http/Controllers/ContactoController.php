629e29357351242ee0f27a04cdfcbaa9e19bcdc2
JohansonCda <jcv9806@hotmail.com>
2025-06-01 20:02:50 -0500

    Laravel practice #4

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -7,8 +7,14 @@ use Illuminate\Http\Request;
 class ContactoController extends Controller
 {
     public function enviar(Request $request){
-        $nombre = request()->input('nombre');
-        $mensaje = request()->input('mensaje');
+
+        $request->validate([
+            'nombre' => 'required|min:3',
+            'mensaje' => 'required|max:255'
+        ]);
+
+        $nombre = $request()->input('nombre');
+        $mensaje = $request()->input('mensaje');
 
         return "Gracias, $nombre. Tu mensaje fue recibido: \"$mensaje\"";
     }
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,13 +6,23 @@
     <title>Document</title>
 </head>
 <body>
+
+    @if ($errors)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li style="color:red">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
     <form method="post" action="/enviar-contacto">
         @csrf
         <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre">
+        <input type="text" name="nombre" value="{{ old('nombre') }}">
         <label for="mensaje">Mensaje:</label>
-        <input type="text" name="mensaje">
+        <input type="text" name="mensaje" value="{{ old('mensaje') }}">
         <input type="submit" value="Enviar">
     </form>
+
+
 </body>
 </html>
